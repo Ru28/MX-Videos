18db3091ce22155ef4063177e081f67e86e8175a
Rupesh Virani <rupeshvirani01@gmail.com>
2022-05-06 19:42:13 +0530

    Initial Commit

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import VideoPost,Comment,UserData
+
+# Register your models here.
+admin.site.register(VideoPost)
+admin.site.register(Comment)
+admin.site.register(UserData)
+
